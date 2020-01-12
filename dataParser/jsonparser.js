71b680fe07ebc6745e82f28b39ec436164498149
z4m2b <z4m2b@ugrad.cs.ubc.ca>
2020-01-11 18:40:46 -0800

    implemented json parser

---
+++ dataParser/jsonparser.js
@@ -0,0 +1,55 @@
+$(document).ready(function () {
+
+    createEntry("Safeway", "tomatoes", "23", "08-01-2020");
+    createEntry("Safeway", "chicken breast", "5", "10-01-2020");
+    createEntry("Safeway", "lettuce", "44", "10-01-2020");
+
+    // $('button').click(function () {
+    //     var toAdd = $("input[name=message]").val();
+    //     $('#messages').append("<p>" + toAdd + "</p>");
+    // });
+
+    submitEntry();
+});
+
+var submission = [];
+
+function createEntry(storeName, food, weight, expiry) {
+
+    //current date
+    var today = new Date();
+    var dateString = n(today.getDate()) + "-" + n((today.getMonth() + 1)) + "-" + today.getFullYear() + " "
+                        + n(today.getHours()) + ":" + n(today.getMinutes());
+
+    //this entry
+    var entry = {
+        store_id : storeName,
+        food_name : food,
+        food_weight : weight,
+        expiry_date : expiry,
+        current_date : dateString
+    };
+
+    //push this entry to submission
+    submission.push(entry);
+}
+
+function submitEntry() {
+    var completedSubmission = submission;
+    submission = [];
+
+    //this is for test
+    for (var i = 0; i < completedSubmission.length; i++) {
+        alert(JSON.stringify(completedSubmission[i]));
+      }
+
+    // return completedSubmission;
+}
+
+function n(num){
+    return num > 9 ? "" + num: "0" + num;
+}
+
+
+
+
