c6a7788b5ce393525365f16395a30cdc3d255e5b
Paul Lam <paullam328@gmail.com>
2020-01-12 12:29:40 -0800

    removed trash code

--- Paul/GoogleMapHandler.js
+++ Paul/GoogleMapHandler.js
@@ -15,9 +15,6 @@ function initMap() {
       query: 'Museum of Contemporary Art Australia',
       fields: ['name', 'geometry'],
     };
-  
-
-    //var service = new google.maps.places.PlacesService(map);
 
     var myPlace = {lat: 49.262440, lng:-123.246231 };
     var service = new google.maps.places.PlacesService(map);
@@ -61,40 +58,3 @@ function getRandomInt(min, max) {
     max = Math.floor(max);
     return Math.floor(Math.random() * (max - min + 1)) + min;
 }
-
-// function callback(results, status) {
-//         if (status === google.maps.places.PlacesServiceStatus.OK) {
-//             alert("OK");
-//           for (var i = 0; i < results.length; i++) {
-//               var infowindow = new google.maps.InfoWindow({
-//                   content: ""
-//                 });
-//               var marker = new google.maps.Marker({
-//                    position: results[0].geometry.location,
-//                    map: map
-//                });
-//                marker.addListener('click', function() {
-//                   infowindow.open(map, marker);
-//                 });
-//               //createMarker(results[0].geometry.location.latlng, "<div>helloworld</div>")
-  
-//               }
-//           // map.setCenter(results[0].geometry.location);
-//           }
-// }
-
-// function createMarker(latlng, html) {
-//     var newmarker = new google.maps.Marker({
-//         position: latlng,
-//         map: map,
-//         title: html
-//     });
-
-//     newmarker['infowindow'] = new google.maps.InfoWindow({
-//             content: html
-//         });
-
-//     google.maps.event.addListener(newmarker, 'mouseover', function() {
-//         this['infowindow'].open(map, this);
-//     });
-// }
